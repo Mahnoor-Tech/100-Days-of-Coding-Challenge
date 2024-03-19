83ab2f9e152dfd62e21065068348c244bd411fdd
Mahnoor <160124465+Mahnoor-Tech@users.noreply.github.com>
2024-03-20 01:20:12 +0500

    100 Days of TypeScript Coding Challenge

---
+++ Question #50-Multiline Template Literals/main.js
@@ -0,0 +1,5 @@
+// Question 50: Multiline Template Literals: Use template literals
+// to create a multiline string that describes your ideal day.
+// Include at least three different activities.
+var my_ideal_day = "This captures my ideal day:\n1. I enjoy starting with an early jog \uD83D\uDEB6\u200D\u2642\uFE0F\n2. I relish coding on personal projects \uD83D\uDC68\u200D\uD83D\uDCBB\n3. I end my day with a good book \uD83D\uDCDA";
+console.log(my_ideal_day);
---
+++ Question #50-Multiline Template Literals/main.ts
@@ -0,0 +1,10 @@
+// Question 50: Multiline Template Literals: Use template literals
+// to create a multiline string that describes your ideal day.
+// Include at least three different activities.
+
+let my_ideal_day = `This captures my ideal day:
+1. I enjoy starting with an early jog 🚶‍♂️
+2. I relish coding on personal projects 👨‍💻
+3. I end my day with a good book 📚`;
+
+console.log(my_ideal_day);
