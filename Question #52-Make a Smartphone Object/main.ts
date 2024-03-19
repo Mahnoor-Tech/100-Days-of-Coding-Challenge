47cefb2f555bdcc64dadf4baacbf8a7b87354d21
Mahnoor <160124465+Mahnoor-Tech@users.noreply.github.com>
2024-03-20 01:21:01 +0500

    100 Days of TypeScript Coding Challenge

---
+++ Question #52-Make a Smartphone Object/main.ts
@@ -0,0 +1,16 @@
+// Question 52: Make a Smartphone Object: Create a simple way to keep
+// track of a smartphone's details. Include its brand, model, and other
+// key features like how much storage it has, the size of its screen,
+// and how long its battery lasts.
+
+let smartphone = {
+  make: "Apple",
+  model: "iPhone 13 Pro",
+  specs: {
+    storage: "256GB",
+    screenSize: "6.1 inches",
+    batteryLife: "Up to 22 hours",
+  },
+};
+
+console.log(smartphone);
